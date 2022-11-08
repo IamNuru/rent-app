fc2a457071da7a222fe9f27ad10cd87541c2bf3b
generous <abdulainurudeentitiaka@gmail.com>
2022-11-07 20:55:08 -0800

    solve home request alignment

--- src/pages/request/HomeRequestCard.js
+++ src/pages/request/HomeRequestCard.js
@@ -6,11 +6,11 @@ import { formatDistance } from 'date-fns'
 const HomeRequestCard = ({ request }) => {
   return (
     <Paper elevation={2} sx={{ width: { xs: '12rem', sm: '18rem' }, minHeight: '10rem' }} className="home-request">
-      <Grid container sx={{ mx: 1, my: 1 }} spacing={2}>
-        <Grid item>
+      <Grid container sx={{pt:1, mx: { sm: 1 } }} spacing={2}>
+        <Grid item xs={12} sx={{ justifyContent: { xs: 'center' } }}>
           <Avatar alt="Requester" src={`${request?.avatarUrl}`} />
         </Grid>
-        <Grid item>
+        <Grid item xs={12}>
           <Typography sx={{ fontSize: '18px', fontWeight: '600' }} className="merriweather">{TruncateSentence(request?.name, 15)}</Typography>
         </Grid>
       </Grid>
@@ -19,9 +19,9 @@ const HomeRequestCard = ({ request }) => {
           TruncateSentence(request?.message, 75)
         }
       </Typography>
-      <Typography sx={{ p: 1, textAlign:'center', color:'lightGray', fontSize:'14px' }}>
+      <Typography sx={{ p: 1, textAlign: 'center', color: 'lightGray', fontSize: '14px' }}>
         {
-          formatDistance(request?.date, new Date(), { addSuffix: true})
+          formatDistance(request?.date, new Date(), { addSuffix: true })
         }
       </Typography>
     </Paper>
